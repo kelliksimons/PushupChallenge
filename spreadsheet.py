b06eb1fd55bd8d5f4bae69d8aa35f96094d39516
The Glass Corner <ksimons@aggies.ncat.edu>
2021-06-07 00:39:48 -0400

    changed color in spreadsheet with python

--- spreadsheet.py
+++ spreadsheet.py
@@ -7,5 +7,14 @@ sh = gc.open_by_key('1Y49BGD-2p9t9WdHkvQwh76q4yI1kP-SYRGPc9wN_SSI')
 worksheet = sh.sheet1
 
 #res = worksheet.get_all_records()
-res = worksheet.get_all_values()
-print(res)
+#res = worksheet.get_all_values()
+#res = worksheet.col_values(1) 
+
+#user = [""]
+#update cell
+#Change cell color
+worksheet.format("D2",{"backgroundColor": 
+{"red": 0.0,
+"green":1.0,
+"blue":0.0}})
+#print(res)#
